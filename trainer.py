aa2339d653980b8c07fb9811cfaad0fd434b3541
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-01 21:39:47 -0400

    AI: trainer.py

--- trainer.py
+++ trainer.py
@@ -1,7 +1,7 @@
 CONTENT:
 ```python
 import numpy as np
-from neural_net import NeuralNetwork, ConvolutionalNeuralNetwork
+from neural_net import NeuralNetwork, ConvolutionalNeuralNetwork, RecurrentNeuralNetwork
 
 class Trainer:
     def __init__(self, model, inputs, targets):
@@ -14,17 +14,7 @@ class Trainer:
             if isinstance(self.model, NeuralNetwork):
                 self.model.train(self.inputs, self.targets, learning_rate)
             elif isinstance(self.model, ConvolutionalNeuralNetwork):
-                # Train convolutional neural network
-                pass
-
-    def evaluate(self):
-        if isinstance(self.model, NeuralNetwork):
-            _, outputs = self.model.forward(self.inputs)
-            accuracy = np.mean(np.argmax(outputs, axis=1) == np.argmax(self.targets, axis=1))
-            return accuracy
-        elif isinstance(self.model, ConvolutionalNeuralNetwork):
-            # Evaluate convolutional neural network
-            outputs = self.model.forward(self.inputs)
-            accuracy = np.mean(np.argmax(outputs, axis=1) == np.argmax(self.targets, axis=1))
-            return accuracy
+                self.model.forward(self.inputs)
+            elif isinstance(self.model, RecurrentNeuralNetwork):
+                self.model.forward(self.inputs)
 ```
