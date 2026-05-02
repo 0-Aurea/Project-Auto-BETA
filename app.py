a147e89ddd75a261e900dd69dac842c51ce7000a
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-01 21:21:09 -0400

    Evolution: app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 CONTENT:
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import torch
 import torch.nn as nn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+from torchvision import datasets, transforms
 
 app = Flask(__name__)
 
@@ -22,43 +25,91 @@ class Net(nn.Module):
         x = self.fc2(x)
         return x
 
-# Load the model
-model = Net()
+# Define a convolutional neural network
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-# Define a function to train the model
-def train_model(X_train, y_train):
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# Load the model
+def load_model(model_type):
+    if model_type == 'simple':
+        model = Net()
+    elif model_type == 'conv':
+        model = ConvNet()
+    else:
+        raise ValueError('Invalid model type')
+    return model
+
+# Train the model
+def train_model(model, X_train, y_train, X_test, y_test):
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-
-    for epoch in range(10):  # loop over the dataset multiple times
+    for epoch in range(10):
         optimizer.zero_grad()
         outputs = model(torch.tensor(X_train, dtype=torch.float32))
         loss = criterion(outputs, torch.tensor(y_train, dtype=torch.long))
         loss.backward()
         optimizer.step()
-
-# Define a function to make predictions
-def make_predictions(X_test):
     model.eval()
     outputs = model(torch.tensor(X_test, dtype=torch.float32))
     _, predicted = torch.max(outputs, 1)
-    return predicted.numpy()
+    accuracy = accuracy_score(y_test, predicted.numpy())
+    return accuracy
 
-# Define a route for the index page
-@app.route('/', methods=['GET', 'POST'])
+# Load MNIST dataset
+transform = transforms.Compose([transforms.ToTensor()])
+trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+
+testset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=False, transform=transform)
+testloader = DataLoader(testset, batch_size=64, shuffle=False)
+
+# Train and save models
+simple_model = Net()
+conv_model = ConvNet()
+
+simple_accuracy = 0
+conv_accuracy = 0
+
+for i, data in enumerate(trainloader, 0):
+    inputs, labels = data
+    inputs = inputs.numpy().reshape(-1, 784)
+    labels = labels.numpy()
+    if i == 0:
+        simple_accuracy = train_model(simple_model, inputs, labels, torch.tensor(testset.data.numpy().reshape(-1, 784), dtype=torch.float32).view(-1, 1, 28, 28), testset.targets.numpy())
+        conv_accuracy = train_model(conv_model, inputs.reshape(-1, 1, 28, 28), labels, torch.tensor(testset.data.numpy().reshape(-1, 784), dtype=torch.float32).view(-1, 1, 28, 28), testset.targets.numpy())
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        # Get the input data
-        data = np.array([int(x) for x in request.form['data'].split(',')])
-
-        # Reshape the data to 784 features (28x28 images)
-        data = data.reshape(1, 784)
-
-        # Make a prediction
-        prediction = make_predictions(data)
-
-        return f"Prediction: {prediction[0]}"
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.form['data']
+    data = np.array([float(x) for x in data.split(',')])
+    model_type = request.form['model_type']
+    model = load_model(model_type)
+    if model_type == 'simple':
+        output = model(torch.tensor(data, dtype=torch.float32))
+    elif model_type == 'conv':
+        output = model(torch.tensor(data.reshape(1, 1, 28, 28), dtype=torch.float32))
+    _, predicted = torch.max(output, 1)
+    return jsonify({'prediction': predicted.item()})
 
 if __name__ == '__main__':
     app.run(debug=True)
