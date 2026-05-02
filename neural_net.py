a0bb5042d93765ed7d1b79ca8215d3826df8b383
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-01 21:28:28 -0400

    AI: neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -17,14 +17,20 @@ class NeuralNetwork:
         return 1 / (1 + np.exp(-x))
 
     def train(self, inputs, targets, learning_rate):
+        # Forward pass
         hidden_layer = self.sigmoid(np.dot(inputs, self.weights1) + self.bias1)
         outputs = self.sigmoid(np.dot(hidden_layer, self.weights2) + self.bias2)
-        output_errors = targets - outputs
-        hidden_errors = output_errors.dot(self.weights2.T)
-        self.weights2 += learning_rate * hidden_layer.T.dot(output_errors)
-        self.bias2 += learning_rate * np.sum(output_errors, axis=0, keepdims=True)
-        self.weights1 += learning_rate * inputs.T.dot(hidden_errors)
-        self.bias1 += learning_rate * np.sum(hidden_errors, axis=0, keepdims=True)
+
+        # Backward pass
+        error = targets - outputs
+        d_output = error * outputs * (1 - outputs)
+        d_hidden = d_output.dot(self.weights2.T) * hidden_layer * (1 - hidden_layer)
+
+        # Weight updates
+        self.weights2 += learning_rate * hidden_layer.T.dot(d_output)
+        self.weights1 += learning_rate * inputs.T.dot(d_hidden)
+        self.bias2 += learning_rate * np.sum(d_output, axis=0, keepdims=True)
+        self.bias1 += learning_rate * np.sum(d_hidden, axis=0, keepdims=True)
 
     def forward(self, inputs):
         hidden_layer = self.sigmoid(np.dot(inputs, self.weights1) + self.bias1)
