1974019dbda6af9b43b9b70f9ecc8a156cfa677f
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-01 21:40:31 -0400

    AI: neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -1,6 +1,9 @@
 CONTENT:
 ```python
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 class NeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size):
@@ -17,57 +20,64 @@ class NeuralNetwork:
         return 1 / (1 + np.exp(-x))
 
     def train(self, inputs, targets, learning_rate):
-        hidden_layer = self.sigmoid(np.dot(inputs, self.weights1) + self.bias1)
-        outputs = self.sigmoid(np.dot(hidden_layer, self.weights2) + self.bias2)
+        for _ in range(1000):
+            hidden_layer = self.sigmoid(np.dot(inputs, self.weights1) + self.bias1)
+            output_layer = self.sigmoid(np.dot(hidden_layer, self.weights2) + self.bias2)
 
-        output_error = targets - outputs
-        output_delta = output_error * outputs * (1 - outputs)
+            output_error = targets - output_layer
+            output_delta = output_error * output_layer * (1 - output_layer)
 
-        hidden_error = output_delta.dot(self.weights2.T)
-        hidden_delta = hidden_error * hidden_layer * (1 - hidden_layer)
+            hidden_error = output_delta.dot(self.weights2.T)
+            hidden_delta = hidden_error * hidden_layer * (1 - hidden_layer)
 
-        self.weights2 += learning_rate * hidden_layer.T.dot(output_delta)
-        self.bias2 += learning_rate * np.sum(output_delta, axis=0, keepdims=True)
+            self.weights2 += learning_rate * hidden_layer.T.dot(output_delta)
+            self.bias2 += learning_rate * np.sum(output_delta, axis=0, keepdims=True)
 
-        self.weights1 += learning_rate * inputs.T.dot(hidden_delta)
-        self.bias1 += learning_rate * np.sum(hidden_delta, axis=0, keepdims=True)
+            self.weights1 += learning_rate * inputs.T.dot(hidden_delta)
+            self.bias1 += learning_rate * np.sum(hidden_delta, axis=0, keepdims=True)
 
 class ConvolutionalNeuralNetwork:
-    def __init__(self, input_channels, hidden_channels, output_size):
-        self.input_channels = input_channels
-        self.hidden_channels = hidden_channels
-        self.output_size = output_size
+    def __init__(self, input_size, hidden_size, output_size):
+        self.conv_net = ConvNet()
 
-        self.weights1 = np.random.rand(input_channels, hidden_channels, 5, 5)
-        self.weights2 = np.random.rand(hidden_channels, output_size, 5, 5)
+    def train(self, inputs, targets, learning_rate):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(self.conv_net.parameters(), lr=learning_rate)
 
-    def forward(self, inputs):
-        outputs = np.zeros((inputs.shape[0], self.output_size, inputs.shape[2] - 4, inputs.shape[3] - 4))
-        for i in range(inputs.shape[0]):
-            for j in range(inputs.shape[1]):
-                for k in range(self.hidden_channels):
-                    for l in range(5):
-                        for m in range(5):
-                            outputs[i, k, j + l, inputs.shape[3] + m] += inputs[i, j, l, m] * self.weights1[j, k, l, m]
-
-        return outputs
+        for epoch in range(1000):
+            optimizer.zero_grad()
+            outputs = self.conv_net(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
 
 class RecurrentNeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size):
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+        self.recurrent_net = RecurrentNet()
 
-        self.weights1 = np.random.rand(input_size, hidden_size)
-        self.weights2 = np.random.rand(hidden_size, output_size)
+    def train(self, inputs, targets, learning_rate):
+        criterion = nn.MSELoss()
+        optimizer = optim.SGD(self.recurrent_net.parameters(), lr=learning_rate)
 
-    def forward(self, inputs):
-        hidden_state = np.zeros((inputs.shape[0], self.hidden_size))
-        outputs = np.zeros((inputs.shape[0], self.output_size))
+        for epoch in range(1000):
+            optimizer.zero_grad()
+            outputs = self.recurrent_net(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
 
-        for i in range(inputs.shape[1]):
-            hidden_state = np.tanh(np.dot(inputs[:, i], self.weights1) + hidden_state)
-            outputs[:, i] = np.dot(hidden_state, self.weights2)
+class Transformer:
+    def __init__(self, input_size, hidden_size, output_size):
+        self.transformer = nn.TransformerEncoderLayer(d_model=input_size, nhead=10, dim_feedforward=hidden_size)
 
-        return outputs
+    def train(self, inputs, targets, learning_rate):
+        criterion = nn.MSELoss()
+        optimizer = optim.SGD(self.transformer.parameters(), lr=learning_rate)
+
+        for epoch in range(1000):
+            optimizer.zero_grad()
+            outputs = self.transformer(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
 ```
