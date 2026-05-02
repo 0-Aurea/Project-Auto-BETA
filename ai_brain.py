4de483c93e66dc3126188b9cd3b273bcf341956e
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-01 21:41:15 -0400

    AI: ai_brain.py

--- ai_brain.py
+++ ai_brain.py
@@ -1,7 +1,7 @@
 CONTENT:
 ```python
 import numpy as np
-from neural_net import NeuralNetwork, ConvolutionalNeuralNetwork, RecurrentNeuralNetwork, Transformer
+from neural_net import NeuralNetwork, ConvolutionalNeuralNetwork, RecurrentNeuralNetwork, Transformer, Autoencoder
 from trainer import Trainer
 
 def main():
@@ -17,23 +17,8 @@ def main():
     # Create trainer
     trainer = Trainer(neural_network, inputs, targets)
 
-    # Train neural network
+    # Train
     trainer.train(0.1, 1000)
-
-    # Test convolutional neural network
-    conv_net = ConvolutionalNeuralNetwork(1, 10, 10)
-    conv_trainer = Trainer(conv_net, inputs, targets)
-    conv_trainer.train(0.1, 1000)
-
-    # Test recurrent neural network
-    recurrent_net = RecurrentNeuralNetwork(1, 10, 1)
-    recurrent_trainer = Trainer(recurrent_net, inputs, targets)
-    recurrent_trainer.train(0.1, 1000)
-
-    # Test transformer
-    transformer_model = Transformer(1, 10, 1)
-    transformer_trainer = Trainer(transformer_model, inputs, targets)
-    transformer_trainer.train(0.1, 1000)
 
 if __name__ == "__main__":
     main()
