b658107d4de586d7e3b97652abe682eea37bae09
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-03 18:41:37 -0400

    refactor: update sw.js

--- sw.js
+++ sw.js
@@ -109,12 +109,12 @@ class JSRewriter {
       return `window.open('${this.rewriteURL(p1)}')`;
     });
 
-    js = js.replace(this.historyPushStateRegex, (match) => {
-      return `history.pushState(${this.rewriteHistoryState(match)})`;
+    js = js.replace(this.historyPushStateRegex, () => {
+      return `history.pushState(${this.rewriteHistoryPushState()})`;
     });
 
-    js = js.replace(this.historyReplaceStateRegex, (match) => {
-      return `history.replaceState(${this.rewriteHistoryState(match)})`;
+    js = js.replace(this.historyReplaceStateRegex, () => {
+      return `history.replaceState(${this.rewriteHistoryReplaceState()})`;
     });
 
     return js;
@@ -124,19 +124,19 @@ class JSRewriter {
     try {
       const evalFunc = new Function(`return ${evalString}`);
       const result = evalFunc();
-      return xorBase64Encode(result.toString(), generateSalt());
+      return result;
     } catch (e) {
       return evalString;
     }
   }
 
-  rewriteFunction(funcString) {
+  rewriteFunction(functionString) {
     try {
-      const func = new Function(`return ${funcString}`);
-      const funcStringified = func.toString();
-      return xorBase64Encode(funcStringified, generateSalt());
+      const func = new Function(`return ${functionString}`);
+      const result = func();
+      return result;
     } catch (e) {
-      return funcString;
+      return functionString;
     }
   }
 
@@ -149,67 +149,150 @@ class JSRewriter {
   }
 
   rewriteURL(url) {
-    const salt = rotatingSalt[rotatingSalt.length - 1];
-    return xorBase64Encode(url, salt);
+    if (url.startsWith('http')) {
+      return url;
+    } else {
+      return xorBase64Encode(url, generateSalt());
+    }
   }
 
-  rewriteHistoryState(state) {
-    try {
-      const stateObj = JSON.parse(state);
-      stateObj.url = this.rewriteURL(stateObj.url);
-      return JSON.stringify(stateObj);
-    } catch (e) {
-      return state;
-    }
+  rewriteHistoryPushState() {
+    return `null, null, '${this.rewriteURL(window.location.href)}'`;
+  }
+
+  rewriteHistoryReplaceState() {
+    return `null, null, '${this.rewriteURL(window.location.href)}'`;
   }
 }
 
-class NexusProxy {
+class HTMLRewriter {
   constructor() {
-    this.jsRewriter = new JSRewriter();
+    this.srcRegex = /src=['"]([^'"]+)['"]/g;
+    this.hrefRegex = /href=['"]([^'"]+)['"]/g;
+    this.actionRegex = /action=['"]([^'"]+)['"]/g;
+    this.srcsetRegex = /srcset=['"]([^'"]+)['"]/g;
+    this.dataRegex = /data-([a-zA-Z0-9-]+)=['"]([^'"]+)['"]/g;
   }
 
-  async handleRequest(request) {
-    const cache = await getCache();
-    const cachedResponse = await cache.match(request);
-    if (cachedResponse) {
-      return cachedResponse;
-    }
+  rewriteHTML(html) {
+    html = html.replace(this.srcRegex, (match, p1) => {
+      return `src='${this.rewriteURL(p1)}'`;
+    });
 
-    try {
-      const response = await fetch(request);
-      const contentType = response.headers.get('Content-Type');
-      if (contentType && contentType.includes('application/javascript')) {
-        const js = await response.text();
-        const rewrittenJs = this.jsRewriter.rewriteJS(js);
-        const rewrittenResponse = new Response(rewrittenJs, {
-          status: response.status,
-          headers: response.headers,
-        });
-        await putResponse(request, rewrittenResponse);
-        return rewrittenResponse;
-      } else {
-        await putResponse(request, response);
-        return response;
-      }
-    } catch (e) {
-      return new Response('Error', {
-        status: 500,
-      });
+    html = html.replace(this.hrefRegex, (match, p1) => {
+      return `href='${this.rewriteURL(p1)}'`;
+    });
+
+    html = html.replace(this.actionRegex, (match, p1) => {
+      return `action='${this.rewriteURL(p1)}'`;
+    });
+
+    html = html.replace(this.srcsetRegex, (match, p1) => {
+      return `srcset='${this.rewriteURL(p1)}'`;
+    });
+
+    html = html.replace(this.dataRegex, (match, p1, p2) => {
+      return `data-${p1}='${this.rewriteURL(p2)}'`;
+    });
+
+    return html;
+  }
+
+  rewriteURL(url) {
+    if (url.startsWith('http')) {
+      return url;
+    } else {
+      return xorBase64Encode(url, generateSalt());
     }
   }
 }
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(new NexusProxy().handleRequest(event.request));
+class CSSRewriter {
+  constructor() {
+    this.urlRegex = /url\(['"]([^'"]+)['"]\)/g;
+    this.importRegex = /@import\s+['"]([^'"]+)['"]/g;
+    this.contentRegex = /content:\s*url\(['"]([^'"]+)['"]\)/g;
+  }
+
+  rewriteCSS(css) {
+    css = css.replace(this.urlRegex, (match, p1) => {
+      return `url('${this.rewriteURL(p1)}')`;
+    });
+
+    css = css.replace(this.importRegex, (match, p1) => {
+      return `@import '${this.rewriteURL(p1)}'`;
+    });
+
+    css = css.replace(this.contentRegex, (match, p1) => {
+      return `content: url('${this.rewriteURL(p1)}')`;
+    });
+
+    return css;
+  }
+
+  rewriteURL(url) {
+    if (url.startsWith('http')) {
+      return url;
+    } else {
+      return xorBase64Encode(url, generateSalt());
+    }
+  }
+}
+
+addEventListener('fetch', async (event) => {
+  event.respondWith(handleRequest(event.request));
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(getCache().then((cache) => cache.keys()).then((keys) => {
-    keys.forEach((key) => cache.delete(key));
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const cache = await getCache();
+  const cachedResponse = await cache.match(request);
+
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  try {
+    const response = await fetch(request);
+    const clonedResponse = response.clone();
+
+    if (response.ok) {
+      const ttlHeader = response.headers.get('ttl');
+      const ttl = ttlHeader ? parseInt(ttlHeader) : 60; // default to 1 minute
+      const cacheResponse = new Response(clonedResponse.body, {
+        status: response.status,
+        headers: response.headers,
+      });
+
+      await putResponse(request, cacheResponse);
+
+      setTimeout(async () => {
+        await deleteResponse(request);
+      }, ttl * 1000);
+
+      return response;
+    } else {
+      return response;
+    }
+  } catch (e) {
+    console.error(e);
+    return new Response('Error', { status: 500 });
+  }
+}
+
+addEventListener('activate', async (event) => {
+  event.waitUntil(caches.keys().then((cacheNames) => {
+    return Promise.all(cacheNames.map((cacheName) => {
+      if (cacheName !== cacheName) {
+        return caches.delete(cacheName);
+      }
+    }));
   }));
 });
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(getCache());
+addEventListener('message', async (event) => {
+  if (event.data.type === 'generateSalt') {
+    generateSalt();
+  }
 });
+self.tf = tf;
