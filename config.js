bbe5428178742a2151b369064bd6f97f3fce44e5
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-03 18:28:50 -0400

    refactor: update config.js

--- config.js
+++ config.js
@@ -1,31 +1,33 @@
 const CACHE_NAME = 'nexus-cache';
 const VERSION = '1.0.0';
 const SALT_ROTATION_INTERVAL = 30 * 60 * 1000; // 30 minutes
+
 const ENCODING_MODES = ['xor', 'base64'];
 const DEFAULT_ENCODING_MODE = 'xor';
+
 const MAX_CACHE_AGE = 24 * 60 * 60 * 1000; // 24 hours
 const PREFETCH_CACHE_AGE = 5 * 60 * 60 * 1000; // 5 hours
+
 const AD_BLOCK_FILTER_LIST_URL = 'https://raw.githubusercontent.com/brave/adblock-lists/master/ublock-origin-default.txt';
 const HOSTS_FILE_URL = 'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts';
+
 const BOOKMARKS_STORAGE_KEY = 'nexus-bookmarks';
 const PROXY_HISTORY_STORAGE_KEY = 'nexus-proxy-history';
 const SETTINGS_STORAGE_KEY = 'nexus-settings';
 
-// HTTPS tunnel configuration
 const SSL_CERT_FILE = 'path/to/ssl/cert.crt';
 const SSL_KEY_FILE = 'path/to/ssl/key.key';
 const HTTPS_PORT = 443;
 const TLS_PROTOCOLS = ['TLSv1.2', 'TLSv1.3'];
 
-// WebSocket configuration
 const WEBSOCKET_TIMEOUT = 30 * 1000; // 30 seconds
 const WEBSOCKET_KEEP_ALIVE_INTERVAL = 10 * 1000; // 10 seconds
+const WEBSOCKET_PING_INTERVAL = 5 * 1000; // 5 seconds
+const WEBSOCKET_PONG_TIMEOUT = 5 * 1000; // 5 seconds
 
-// Cookie scoping configuration
 const COOKIE_SCOPING_ENABLED = true;
 const COOKIE_STORAGE_KEY = 'nexus-cookies';
 
-// Header rewriting rules
 const HEADER_REWRITE_RULES = {
   'Content-Security-Policy': (value) => value.replace('default-src', 'script-src \'self\''),
   'X-Frame-Options': (value) => value.replace('SAMEORIGIN', '*'),
@@ -33,22 +35,24 @@ const HEADER_REWRITE_RULES = {
   'X-Content-Type-Options': (value) => value.replace('nosniff', ''),
 };
 
-// Integrated HTTPS tunnel and WebSocket settings
 const INTEGRATED_HTTPS_TUNNEL = true;
 const WEBSOCKET_UPGRADE_PROXYING = true;
 
-// Cache API settings
 const CACHE_TTL_HEADERS = ['Cache-Control', 'Expires'];
 const CACHE_REVALIDATE_THRESHOLD = 5 * 60 * 1000; // 5 minutes
+const CACHE_SIZE_LIMIT = 100 * 1024 * 1024; // 100MB
 
-// Prefetch hints settings
 const PREFETCH_HINTS_ENABLED = true;
 const PREFETCH_HINTS_CACHE_AGE = 24 * 60 * 60 * 1000; // 24 hours
+
 const COMPRESSION_ENABLED = true;
 const BROTLI_COMPRESSION_THRESHOLD = 1024; // bytes
 const GZIP_COMPRESSION_THRESHOLD = 1024; // bytes
 const COMPRESSION_MIN_SIZE = 100; // bytes
 const COMPRESSION_MAX_SIZE = 10 * 1024 * 1024; // 10MB
+
+const CONNECTION_TIMEOUT = 10 * 1000; // 10 seconds
+const SOCKET_TIMEOUT = 30 * 1000; // 30 seconds
 
 module.exports = {
   CACHE_NAME,
@@ -69,6 +73,8 @@ module.exports = {
   TLS_PROTOCOLS,
   WEBSOCKET_TIMEOUT,
   WEBSOCKET_KEEP_ALIVE_INTERVAL,
+  WEBSOCKET_PING_INTERVAL,
+  WEBSOCKET_PONG_TIMEOUT,
   COOKIE_SCOPING_ENABLED,
   COOKIE_STORAGE_KEY,
   HEADER_REWRITE_RULES,
@@ -76,6 +82,7 @@ module.exports = {
   WEBSOCKET_UPGRADE_PROXYING,
   CACHE_TTL_HEADERS,
   CACHE_REVALIDATE_THRESHOLD,
+  CACHE_SIZE_LIMIT,
   PREFETCH_HINTS_ENABLED,
   PREFETCH_HINTS_CACHE_AGE,
   COMPRESSION_ENABLED,
@@ -83,4 +90,6 @@ module.exports = {
   GZIP_COMPRESSION_THRESHOLD,
   COMPRESSION_MIN_SIZE,
   COMPRESSION_MAX_SIZE,
+  CONNECTION_TIMEOUT,
+  SOCKET_TIMEOUT,
 };
