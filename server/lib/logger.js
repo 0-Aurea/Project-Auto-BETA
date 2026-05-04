c2a7dd33f220922fe107fe91eb77e2791b79db52
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-03 20:11:45 -0400

    feat: create server/lib/logger.js

---
+++ server/lib/logger.js
@@ -0,0 +1,24 @@
+const winston = require('winston');
+const { combine, timestamp, label, printf } = winston.format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    label({ label: 'NEXUS' }),
+    myFormat,
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+module.exports = logger;
