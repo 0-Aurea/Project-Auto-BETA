599d968d4b7903ca4e98837401affd0c84d30d20
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-08 17:48:04 -0400

    refactor: update server/index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 const WebSocket = require('ws');
+const url = require('url');
 const config = require('./lib/config');
 const logger = require('./lib/logger');
 const proxyEngine = require('./lib/proxyEngine');
@@ -30,7 +31,25 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/service/:encodedUrl', authMiddleware.authenticate, proxyEngine);
+app.get('/service/:encodedUrl', authMiddleware.authenticate, async (req, res, next) => {
+  try {
+    const encodedUrl = req.params.encodedUrl;
+    const targetUrl = Buffer.from(encodedUrl, 'base64').toString('utf8');
+    const response = await proxyEngine(targetUrl);
+    Object.keys(response.headers).forEach((header) => {
+      if (header === 'content-security-policy' || header === 'strict-transport-security' || header === 'x-frame-options') {
+        delete response.headers[header];
+      } else if (header === 'location' || header === 'set-cookie') {
+        response.headers[header] = response.headers[header].replace('http://', 'https://').replace('https://', '');
+      }
+    });
+    res.writeHead(response.statusCode, response.headers);
+    res.end(response.body);
+  } catch (error) {
+    logger.error('Error:', error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+});
 
 app.use((err, req, res, next) => {
   logger.error('Error:', err);
@@ -41,13 +60,19 @@ httpServer.listen(config.server.port, config.server.host, () => {
   logger.info(`HTTP server listening on ${config.server.host}:${config.server.port}`);
 });
 
-httpsServer.listen(config.server.https.port, config.server.host, () => {
-  logger.info(`HTTPS server listening on ${config.server.host}:${config.server.https.port}`);
+httpsServer.listen(443, config.server.host, () => {
+  logger.info(`HTTPS server listening on ${config.server.host}:443`);
 });
 
 wss.on('connection', (ws, req) => {
-  const targetUrl = req.url.split('?')[0].slice(1);
-  const targetOptions = url.parse(targetUrl);
+  const targetUrl = url.parse(req.url, true);
+  const targetOptions = {
+    hostname: targetUrl.hostname,
+    port: 443,
+    path: targetUrl.path,
+    method: 'GET',
+    headers: req.headers,
+  };
   const targetSocket = https.request(targetOptions, (res) => {
     ws.send(res.headers['sec-websocket-accept']);
     res.pipe(ws);
