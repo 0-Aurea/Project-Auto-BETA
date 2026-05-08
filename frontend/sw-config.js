a1644641820004283a7959016890162146fc42d1
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-08 18:03:07 -0400

    refactor: update frontend/sw-config.js

--- frontend/sw-config.js
+++ frontend/sw-config.js
@@ -1,9 +1,12 @@
 export const PREFIX = '/service/';
 
+const KEY = 'nexus-proxy';
+
 export function encode(url) {
   const encoder = new TextEncoder();
   const uint8Array = encoder.encode(url);
-  const base64String = btoa(String.fromCharCode(...uint8Array));
+  const encrypted = xor(uint8Array, KEY);
+  const base64String = btoa(String.fromCharCode(...encrypted));
   const safeBase64String = base64String.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
   return PREFIX + safeBase64String;
 }
@@ -14,7 +17,16 @@ export function decode(encodedStr) {
   }
   const safeBase64String = encodedStr.substring(PREFIX.length);
   const paddedBase64String = safeBase64String + '='.repeat((4 - safeBase64String.length % 4) % 4);
-  const uint8Array = new Uint8Array(atob(paddedBase64String.replace(/-/g, '+').replace(/_/g, '/')).split('').map(c => c.charCodeAt(0)));
+  const decrypted = new Uint8Array(atob(paddedBase64String.replace(/-/g, '+').replace(/_/g, '/')).split('').map(c => c.charCodeAt(0)));
   const decoder = new TextDecoder();
-  return decoder.decode(uint8Array);
+  return decoder.decode(xor(decrypted, KEY));
+}
+
+function xor(arr, key) {
+  const keyBytes = new TextEncoder().encode(key);
+  const result = new Uint8Array(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i] ^ keyBytes[i % keyBytes.length];
+  }
+  return result;
 }
