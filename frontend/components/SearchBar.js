e2d9cc9ea36beb2d6bd343c1b5b71601066771dd
Aurea <fmrjsqfdz7@privaterelay.appleid.com>
2026-05-09 11:23:15 -0400

    refactor: update frontend/components/SearchBar.js

--- frontend/components/SearchBar.js
+++ frontend/components/SearchBar.js
@@ -124,9 +124,9 @@ export class SearchBar {
   handleSearchEngineChange(engine) {
     this.state.searchEngine = engine;
     localStorage.setItem('searchEngine', engine);
-    this.searchEngineSelect.children[0].classList.toggle('active');
-    this.searchEngineSelect.children[1].classList.toggle('active');
-    this.slider.classList.toggle('active-google');
-    this.slider.classList.toggle('active-bing');
+    this.searchEngineSelect.children[0].classList.toggle('active', engine === 'google');
+    this.searchEngineSelect.children[1].classList.toggle('active', engine === 'bing');
+    this.slider.classList.remove(`active-google`, `active-bing`);
+    this.slider.classList.add(`active-${engine}`);
   }
 }
